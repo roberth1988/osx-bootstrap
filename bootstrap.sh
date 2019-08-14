bed422aa6c1c44898e22312096fa0d0582f8e5da
Robert Hoppe <rh@21sheeps.com>
2019-08-14 13:37:01 +0200

    Add install for fzf

--- bootstrap.sh
+++ bootstrap.sh
@@ -213,7 +213,7 @@ dialog --title "Install dotfiles" \
 
 response=$?
 case $response in
-   0) ellipsis install roberth1988/files;;
+   0) ellipsis install roberth1988/files &&  $(brew --prefix)/opt/fzf/install;;
    1) echo "Boostrapping done";;
    255) echo "[ESC] key pressed.";;
 esac
